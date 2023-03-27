f6cf65dc8ad583b1d4658c4b848ff10dbbbce7d8
Keiichi Tsuda <keiichi.tsuda@gmail.com>
2023-03-27 12:47:36 +0900

    fix bug

--- script/install.sh
+++ script/install.sh
@@ -4,26 +4,28 @@ set -e -o pipefail
 ##
 ## install custom quotelets
 ##
-readonly FQ_ROOT=`dirname $(perldoc -l Finance::Quote)`
+readonly PROJ_DIR=$(dirname ${BASH_SOURCE:-$0})/../
+readonly FQ_ROOT=$(dirname $(perldoc -l Finance::Quote))
 readonly DO_TEST=false
 
 # copy quotelets
 #QUOTELETS="YahooJapanA.pm YahooJapanB.pm"
 QUOTELETS="YahooJapanB.pm" # YahooJapanA is outdated. (2023/3/5)
 for QUOTELET in $QUOTELETS; do
-  sudo cp $(dirname $BASH_SOURCE)/../lib/Finance/Quote/${QUOTELET} ${FQ_ROOT}/Quote
+  sudo cp ${PROJ_DIR}/lib/Finance/Quote/${QUOTELET} ${FQ_ROOT}/Quote
   sudo chmod 444 ${FQ_ROOT}/Quote/${QUOTELET}
   
   # mac
-  sudo chown root:wheel ${FQ_ROOT}/Quote/${QUOTELET}
+  #sudo chown root:wheel ${FQ_ROOT}/Quote/${QUOTELET}
   # linux
-  #sudo chown root:root ${FQ_ROOT}/Quote/${QUOTELET}
+  sudo chown root:root ${FQ_ROOT}/Quote/${QUOTELET}
 done
 
 # update Quote.pm
-sudo perl updateFQ.pl ${FQ_ROOT}/Quote.pm "${QUOTELETS}"
+sudo perl ${PROJ_DIR}/script/updateFQ.pl ${FQ_ROOT}/Quote.pm "${QUOTELETS}"
 
 # test quotelets
 if "${DO_TEST}"; then
+  cd ${PROJ_DIR}
   prove t
 fi
